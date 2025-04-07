51f2fc4287d9f8638905d732b8808d56c9395e31
Kenneth Graham <ken@kmghub.com>
2025-04-06 21:15:52 -0400

    console log for troubleshooting

--- src/pages/BlogPost.jsx
+++ src/pages/BlogPost.jsx
@@ -1,22 +1,22 @@
-import React from 'react';
+// src/pages/BlogPost.jsx
+import { useEffect, useState } from 'react';
 import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
-import { useEffect, useState } from 'react';
 
-const BlogPost = () => {
+export default function BlogPost() {
   const [content, setContent] = useState('');
 
   useEffect(() => {
-    fetch('/src/blog/first-post.md')
+    fetch('/blog/first-post.md')
       .then((res) => res.text())
       .then((text) => setContent(text));
   }, []);
 
   return (
     <div className="container py-5">
-      <ReactMarkdown remarkPlugins={[remarkGfm]}>{content}</ReactMarkdown>
+      <ReactMarkdown remarkPlugins={[remarkGfm]}>
+        {content}
+      </ReactMarkdown>
     </div>
   );
-};
-
-export default BlogPost;
+}
