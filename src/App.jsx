549abc541dbbb4873071d3e24a9bfcb300db78ce
Kenneth Graham <ken@kmghub.com>
2025-04-08 20:35:01 -0400

    Completed route to fetch projects from Supabase and redering Project Cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
 // src/App.jsx
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import TestSupabase from './pages/TestSupabase';
+import Projects from './pages/Projects';
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/supabase-test" element={<TestSupabase />} />
       <Route path="/projects" element={<Projects />} />
-
     </Routes>
   );
 }
